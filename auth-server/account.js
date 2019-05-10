b066e9c843089789634cab59d08b52998530f8c3
cmillani <carloseduardomillani@gmail.com>
2019-05-09 22:30:06 -0300

    Sign in and up working, plaintext password

--- auth-server/account.js
+++ auth-server/account.js
@@ -1,7 +1,9 @@
-const assert = require('assert');
 const _ = require('lodash');
 const crypto = require("crypto");
-const MongoClient = require("mongodb").MongoClient;
+const mongodb = require("mongodb")
+const MongoClient = mongodb.MongoClient;
+const ObjectId = mongodb.ObjectId;
+
 var db = null;
 
 (async () => {
@@ -12,17 +14,6 @@ var db = null;
   console.log(`DB connection err: ${err}`);
 });
 
-var USERS = {
-  '23121d3c-84df-44ac-b458-3d63a9a05497': {
-    email: 'foo@example.com',
-    email_verified: true,
-  },
-  'c2ac2b4a-2262-4e2f-847a-a40dd3c4dcd5': {
-    email: 'bar@example.com',
-    email_verified: false,
-  },
-};
-
 class Account {
   constructor(id) {
     this.accountId = id; // the property named accountId is important to oidc-provider
@@ -31,7 +22,7 @@ class Account {
   // claims() should return or resolve with an object with claims that are mapped 1:1 to
   // what your OP supports, oidc-provider will cherry-pick the requested ones automatically
   claims() {
-    return Object.assign({}, USERS[this.accountId], {
+    return Object.assign({}, {
       sub: this.accountId,
     });
   }
@@ -39,37 +30,43 @@ class Account {
   static async findById(ctx, id) {
     // this is usually a db lookup, so let's just wrap the thing in a promise, oidc-provider expects
     // one
-    return new Account(id);
+    let user = await db.collection('usersCollection').findOne({"_id" : ObjectId(id)});
+    if (user != null) {
+      return new Account(id);
+    } else {
+      return null;
+    }
   }
 
   static async authenticate(email, password) {
-    assert(password, 'password must be provided');
-    assert(email, 'email must be provided');
+    if (password == null) throw 'password must be provided';
+    if (email == null) throw 'email must be provided';
     const lowercasedEmail = String(email).toLowerCase();
-    // const id = _.findKey(USERS, { email: lowercased });
-    // assert(id, 'invalid credentials provided');
 
     return db.collection('usersCollection').findOne({'email': lowercasedEmail}).then( user => {
       if (user != null && user.password == password) {
         return new this(user._id);
       } else {
-        throw "Throw not found"
+        throw "User Not Found";
       }
     })
   }
 
   static async create(email, password) {
-    assert(password, 'password must be provided');
-    assert(email, 'email must be provided');
-    const lowercased = String(email).toLowerCase();
-    const id = crypto.randomBytes(16).toString("hex");
-
-    USERS[id] = {
-      email: lowercased,
-      email_verified: false
+    if (password == null) throw 'password must be provided';
+    if (email == null) throw 'email must be provided';
+    const lowercasedEmail = String(email).toLowerCase();
+    
+    let user = await db.collection('usersCollection').findOne({'email': lowercasedEmail});
+    if (user == null) {
+      let result = await db.collection('usersCollection').insertOne({
+        email: lowercasedEmail,
+        password: password
+      });
+      return new this(result.insertedId);
+    } else {
+      throw `User already registered`;
     }
-
-    return new this(id);
   }
 }
 
